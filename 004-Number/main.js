e90c3ac1c0b6d6ef12838ce9f2d58d4424a5f81a
İbrahim Karabulut <job.ibrahimkarabulut@gmail.com>
2023-09-19 13:48:19 +0300

    Number finished

---
+++ 004-Number/main.js
@@ -0,0 +1,33 @@
+/*
+
++   Plus
+-   Minus
+*   Impact
+/   Divide
+%   Remainder
+**  Power of
+
+*/
+
+// Mathematical expressions
+console.log("6 + 12 = ", 6 + 12);
+console.log("12 - 7 = ", 12 - 7);
+console.log("8 * 3  = ", 8 * 3);
+console.log("3 ** 3 = ", 3 ** 3);
+console.log("81 / 9 = ", 81 / 9);
+console.log("89 % 9 = ", 89 % 9);
+
+// Logical expressions
+let numberValue = 25;
+numberValue = numberValue+1;
+numberValue += 1;
+numberValue++;
+console.log(numberValue);
+
+let remainderNumber = 37;
+remainderNumber%=3;
+console.log(remainderNumber);
+
+let powerOfNumber = 3;
+powerOfNumber**=3;
+console.log(powerOfNumber);
