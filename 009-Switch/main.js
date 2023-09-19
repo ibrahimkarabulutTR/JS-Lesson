a422e1bcc0ed757611a965e2f6854eb5508a78ad
İbrahim Karabulut <job.ibrahimkarabulut@gmail.com>
2023-09-19 15:43:39 +0300

    Switch finished

---
+++ 009-Switch/main.js
@@ -0,0 +1,16 @@
+let result = 'AA';
+
+switch(result) {
+	case 'AA':
+		console.log('Value is AA');
+		break;
+	case 'BB':
+		console.log('Value is BB');
+		break;
+	case 'CC':
+		console.log('Value is CC');
+		break;
+	default:
+		console.log('Undefined');
+		break;
+}
