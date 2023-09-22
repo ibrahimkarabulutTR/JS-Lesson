00aea325b539f0ec3cf12b311b7fb42880c01abf
İbrahim Karabulut <job.ibrahimkarabulut@gmail.com>
2023-09-22 18:23:42 +0300

    EditCSS finished

---
+++ 019-EditCSS/main.js
@@ -0,0 +1,9 @@
+const head = document.querySelector('h1#head');
+
+// This way it overwrite of the div
+// head.setAttribute('style', 'background:red');
+
+head.style.backgroundColor = 'blue';
+head.style.margin = '0';
+head.style.textAlign = 'center';
+head.style.padding = '50px';
