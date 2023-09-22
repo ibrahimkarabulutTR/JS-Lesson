eec2889b22f388fa04e56d22b7c47e5a02ff9ee9
İbrahim Karabulut <job.ibrahimkarabulut@gmail.com>
2023-09-22 17:50:59 +0300

    Math finished

---
+++ 014-Math/main.js
@@ -0,0 +1,21 @@
+console.log(Math);
+
+// Print PI
+console.log(Math.PI);
+
+// To round to a whole number
+console.log(Math.round(5.3));
+console.log(Math.round(5.8));
+
+// To round to the lower integer
+console.log(Math.floor(5.8));
+
+// To round to the upper integer
+console.log(Math.ceil(5.8));
+
+// To delete the decimal of a number
+console.log(Math.trunc(5.8));
+
+// Create a random number
+console.log(Math.random());
+console.log(Math.random()*10);
